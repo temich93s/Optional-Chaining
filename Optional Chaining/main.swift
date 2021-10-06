527652ac84747fa59dee57edcfbe3cfc71391fee
Temich93s <nil>
2021-10-07 09:07:40 +1000

    Пройдена тема: Определение классовых моделей для ОП

--- Optional Chaining/main.swift
+++ Optional Chaining/main.swift
@@ -42,4 +42,50 @@ if let roomCount = john.residence?.numberOfRooms {
 // Выведет "John's residence has 1 room(s)."
 
 
+//MARK: Определение классовых моделей для ОП
+print("\n//Определение классовых моделей для ОП")
 
+class Person1 {
+    var residence: Residence?
+}
+
+class Residence1 {
+    var rooms = [Room1]()
+    var numberOfRooms: Int {
+        return rooms.count
+    }
+    subscript(i: Int) -> Room1 {
+        get {
+            return rooms[i]
+        }
+        set {
+            rooms[i] = newValue
+        }
+    }
+    func printNumberOfRooms() {
+        print("Общее количество комнат равно \(numberOfRooms)")
+    }
+    var address: Address1?
+}
+
+class Room1 {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Address1 {
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    func buildingIdentifier() -> String? {
+        if let buildingNumber = buildingNumber, let street = street {
+            return "\(buildingNumber) \(street)"
+        } else if buildingName != nil {
+            return buildingName
+        } else {
+            return nil
+        }
+    }
+}
