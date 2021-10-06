4deb1227779ce4d6b24bd212a2c4c97952886c7d
Temich93s <nil>
2021-10-07 09:07:12 +1000

    Пройдена тема: ОП как альтернатива принудительному извлечению

--- Optional Chaining/main.swift
+++ Optional Chaining/main.swift
@@ -9,3 +9,37 @@ import Foundation
 
 print("Hello, World!")
 
+//MARK: ОП как альтернатива принудительному извлечению
+print("\n//ОП как альтернатива принудительному извлечению")
+
+class Person {
+    var residence: Residence?
+}
+ 
+class Residence {
+    var numberOfRooms = 1
+}
+
+let john = Person()
+
+//let roomCount = john.residence!.numberOfRooms
+// ошибка runtime
+
+if let roomCount = john.residence?.numberOfRooms {
+    print("John's residence has \(roomCount) room(s).")
+} else {
+    print("Unable to retrieve the number of rooms.")
+}
+// Выведет "Unable to retrieve the number of rooms."
+
+john.residence = Residence()
+
+if let roomCount = john.residence?.numberOfRooms {
+    print("John's residence has \(roomCount) room(s).")
+} else {
+    print("Unable to retrieve the number of rooms.")
+}
+// Выведет "John's residence has 1 room(s)."
+
+
+
